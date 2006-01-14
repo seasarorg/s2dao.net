43378da9ba3efd983f16e6e9ce6ff0c6611d3b8d
fujiihiroaki <fujiihiroaki@638a8f4c-ee01-0410-9780-f2e338a3b7db>
2006-01-14 18:00:32 +0000

    Firebird対応の修正
    git-svn-id: https://www.seasar.org/svn/s2dao.net/trunk@244 638a8f4c-ee01-0410-9780-f2e338a3b7db

--- s2dao.net/source/Seasar.Dao/Seasar.Framework.Util/DataProviderUtil.cs
+++ s2dao.net/source/Seasar.Dao/Seasar.Framework.Util/DataProviderUtil.cs
@@ -1,4 +1,5 @@
 #region Copyright
+
 /*
  * Copyright 2005 the Seasar Foundation and the Others.
  *
@@ -14,46 +15,58 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+
 #endregion
 
-using System;
 using System.Data;
 
 namespace Seasar.Framework.Util
 {
-	/// <summary>
-	/// DataProviderUtil
-	/// </summary>
-	public sealed class DataProviderUtil
-	{
-		private DataProviderUtil()
-		{
-		}
+    /// <summary>
+    /// DataProviderUtil
+    /// </summary>
+    public sealed class DataProviderUtil
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private DataProviderUtil()
+        {
+            ;
+        }
 
+        /// <summary>
+        /// バインド変数タイプを取得する
+        /// </summary>
+        /// <param name="cn">コネクションオブジェクト</param>
+        /// <returns>バインド変数タイプ</returns>
         public static BindVariableType GetBindVariableType(IDbConnection cn)
         {
-            if("SqlConnection".Equals(cn.GetType().Name) || 
-                "DB2Connection".Equals(cn.GetType().Name) ||
-				"FbConnection".Equals(cn.GetType().Name))
+            if ( "SqlConnection".Equals(cn.GetType().Name) ||
+                "DB2Connection".Equals(cn.GetType().Name) )
             {
                 return BindVariableType.AtmarkWithParam;
             }
-            else if("OracleConnection".Equals(cn.GetType().Name))
+            else if ( "OracleConnection".Equals(cn.GetType().Name) )
             {
                 return BindVariableType.ColonWithParam;
             }
-            else if("MySqlConnection".Equals(cn.GetType().Name))
+            else if ( "MySqlConnection".Equals(cn.GetType().Name) )
             {
                 return BindVariableType.QuestionWithParam;
             }
-			else if("NpgsqlConnection".Equals(cn.GetType().Name))
-			{
-				return BindVariableType.ColonWithParamToLower;
-			}
+            else if ( "NpgsqlConnection".Equals(cn.GetType().Name) )
+            {
+                return BindVariableType.ColonWithParamToLower;
+            }
+            else if ( "FbConnection".Equals(cn.GetType().Name) )
+            {
+                return BindVariableType.Question;
+            }
             else
             {
                 return BindVariableType.Question;
             }
         }
-	}
+    }
 }
