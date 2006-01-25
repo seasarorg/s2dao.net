95bc7b68f9f5c59fea7ea3e939491c6d67b3769c
sugimotokazuya <sugimotokazuya@638a8f4c-ee01-0410-9780-f2e338a3b7db>
2006-01-25 15:51:06 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.net/trunk@247 638a8f4c-ee01-0410-9780-f2e338a3b7db

--- s2dao.net/data/FirebirdDemo.sql
+++ s2dao.net/data/FirebirdDemo.sql
@@ -2,6 +2,7 @@ SET SQL DIALECT 3;
 
 CREATE DATABASE 'C:\S2DEMO.FDB'
 PAGE_SIZE 4096
+user 'SYSDBA' password 'masterkey'
 DEFAULT CHARACTER SET UNICODE_FSS;
 
 /* Table: EMP2, Owner: SYSDBA */
@@ -15,9 +16,16 @@ CREATE TABLE "EMP2"
 );
 
 
-/* Grant Roles for this database */
+/* Use GSEC (Create User)======================================== */
+/* \FireBird_1_5\bin\gsec.exe -user sysdba -password masterkey    */
+/* GSEC> add S2DEMO -pw s2demo                                    */
+/* GSEC> quit                                                     */
+/* End GSEC ===================================================== */
 
 
 /* Grant permissions for this database */
-
 GRANT DELETE, INSERT, SELECT, UPDATE, REFERENCES ON "EMP2" TO S2DEMO;
+
+
+INSERT INTO EMP2 VALUES(7369,'SMITH',20);
+INSERT INTO EMP2 VALUES(7499,'ALLEN',30);
