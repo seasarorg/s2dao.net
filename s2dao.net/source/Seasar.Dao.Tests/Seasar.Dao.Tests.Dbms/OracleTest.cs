d2692f892fa45d5e24b83f1a3b0bc3324483ceab
sugimotokazuya <sugimotokazuya@638a8f4c-ee01-0410-9780-f2e338a3b7db>
2006-03-26 15:44:32 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.net/trunk@309 638a8f4c-ee01-0410-9780-f2e338a3b7db

---
+++ s2dao.net/source/Seasar.Dao.Tests/Seasar.Dao.Tests.Dbms/OracleTest.cs
@@ -0,0 +1,66 @@
+#region Copyright
+/*
+ * Copyright 2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+#endregion
+
+using System;
+using Seasar.Dao.Impl;
+using Seasar.Dao.Dbms;
+using Seasar.Extension;
+using Seasar.Extension.ADO;
+using Seasar.Extension.ADO.Impl;
+using Seasar.Extension.Unit;
+using MbUnit.Framework;
+
+namespace Seasar.Dao.Tests.Dbms
+{
+	[TestFixture]
+	public class OracleTest : S2TestCase {
+
+		[Test, S2]
+		public void TestCreateAutoSelectList() {
+            
+            IDataSource dataSource = (IDataSource) GetComponent(typeof(IDataSource));
+			
+            IDbms dbms = new Oracle();
+			
+            IBeanMetaData bmd = new BeanMetaDataImpl(typeof(Employee),
+					new DatabaseMetaDataImpl(dataSource), dbms);
+			
+            String sql = dbms.GetAutoSelectSql(bmd);
+			
+            Assert.AreEqual("SELECT EMP2.DEPTNUM, EMP2.ENAME, EMP2.EMPNO FROM EMP2", sql);
+
+		}
+
+        [Test, S2]
+        public void TestCreateAutoSelectList2() 
+        {
+            
+            IDataSource dataSource = (IDataSource) GetComponent(typeof(IDataSource));
+			
+            IDbms dbms = new Oracle();
+			
+            IBeanMetaData bmd = new BeanMetaDataImpl(typeof(Department),
+                new DatabaseMetaDataImpl(dataSource), dbms);
+			
+            String sql = dbms.GetAutoSelectSql(bmd);
+			
+            Assert.AreEqual("SELECT DEPT.LOC, DEPT.DNAME, DEPT.DEPTNO, DEPT.VERSIONNO FROM DEPT", sql);
+
+        }
+	}
+}
