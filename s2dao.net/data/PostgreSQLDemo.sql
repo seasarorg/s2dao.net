6de8c6ef95b2019a1a189dcb85a52626893359c2
sugimotokazuya <sugimotokazuya@638a8f4c-ee01-0410-9780-f2e338a3b7db>
2006-04-17 15:51:45 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.net/trunk@312 638a8f4c-ee01-0410-9780-f2e338a3b7db

--- s2dao.net/data/PostgreSQLDemo.sql
+++ s2dao.net/data/PostgreSQLDemo.sql
@@ -14,6 +14,21 @@ CREATE TABLE emp2 (
 ALTER TABLE ONLY emp2
     ADD CONSTRAINT emp2_pkey PRIMARY KEY (empno);
 
+CREATE SEQUENCE "sequencetable_seqcol_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE "sequencetable_seqcol_seq" OWNER TO s2demouser;
+
+CREATE TABLE "SequenceTable"
+(
+  "SeqCol" int4 NOT NULL DEFAULT nextval('sequencetable_seqcol_seq'::regclass)
+) 
+WITHOUT OIDS;
+ALTER TABLE "SequenceTable" OWNER TO s2demouser;
+
 INSERT INTO emp2 (empno, ename, deptnum) VALUES (7369, 'SMITH', 20);
 INSERT INTO emp2 (empno, ename, deptnum) VALUES (7499, 'ALLEN', 30);
 
